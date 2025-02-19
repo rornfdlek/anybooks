b97d5a41c5c8cd04bb00a57fc2128af8cb8c95a1
rornfdlek <rornfdlek@naver.com>
2025-02-19 13:08:01 +0900

    bedrock model invocation logs expert chat

--- admin/manual_app.py
+++ admin/manual_app.py
@@ -5,19 +5,46 @@ import admin.manual_lib as glib
 
 st.title("💁🏻 Chat with AWS Expert")
 
-st.header("Ask anything about Amazon Bedrock!")
+# 탭 생성
+tab1, tab2 = st.tabs([
+    "Log Summary",
+    "AWS Expert Chat"
+])
 
-input_text = st.text_area("Input text", label_visibility="collapsed") #레이블이 없는 여러 줄 텍스트 상자를 표시
-go_button = st.button("Ask Expert", type="primary") #기본 버튼을 표시
+with tab1:
+    st.header("Amazon Bedrock Model Invocation Log Summary")
+    st.write("베드락 Model invocation 로그를 요약해드립니다.")
 
-if go_button: #버튼을 클릭하면 이 if 블록의 코드가 실행됩니다
-    
-    with st.spinner("Working..."): #이 with 블록의 코드가 실행되는 동안 스피너를 표시합니다
-        response_content, search_text = glib.get_rag_response2(
-            query=input_text
-            ) #지원 라이브러리를 통해 모델을 호출
+    input_text = st.text_area("Input text1", label_visibility="collapsed") #레이블이 없는 여러 줄 텍스트 상자를 표시
+    go_button = st.button("Summarize", type="primary") #기본 버튼을 표시
+
+    if go_button: #버튼을 클릭하면 이 if 블록의 코드가 실행됩니다
         
-        st.write(response_content) #응답 콘텐츠 표시
+        with st.spinner("Working..."): #이 with 블록의 코드가 실행되는 동안 스피너를 표시합니다
+            response_content, search_text = glib.get_rag_response1(
+                query=input_text
+                ) #지원 라이브러리를 통해 모델을 호출
+            
+            st.write(response_content) #응답 콘텐츠 표시
+            
+            with st.expander("See search results"):
+                st.write(search_text)
+
+with tab2: 
+    st.header("Ask anything about Amazon Bedrock!")
+    st.write("베드락, 무엇이든 물어보세요!")
+
+    input_text = st.text_area("Input text2", label_visibility="collapsed") #레이블이 없는 여러 줄 텍스트 상자를 표시
+    go_button = st.button("Ask Expert", type="primary") #기본 버튼을 표시
+
+    if go_button: #버튼을 클릭하면 이 if 블록의 코드가 실행됩니다
         
-        with st.expander("See search results"):
-            st.write(search_text)
+        with st.spinner("Working..."): #이 with 블록의 코드가 실행되는 동안 스피너를 표시합니다
+            response_content, search_text = glib.get_rag_response2(
+                query=input_text
+                ) #지원 라이브러리를 통해 모델을 호출
+            
+            st.write(response_content) #응답 콘텐츠 표시
+            
+            with st.expander("See search results"):
+                st.write(search_text)
--- admin/manual_lib.py
+++ admin/manual_lib.py
@@ -6,7 +6,9 @@ bedrock_client = session.client(service_name='bedrock-runtime', region_name='us-
 kb_client = session.client(service_name='bedrock-agent-runtime', region_name='us-west-2')
 
 # Bedrock Knowledge Base ID
-KNOWLEDGE_BASE_ID = 'IRVM6NCZIN'
+
+KNOWLEDGE_BASE_ID1 = '1HX6TLM14C'
+KNOWLEDGE_BASE_ID2 = 'IRVM6NCZIN'
 
 # 사용할 LLM Model ARN
 MODEL_ARN = 'arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-v2:1'
@@ -24,7 +26,7 @@ def get_rag_response(query):
         retrieveAndGenerateConfiguration={
             'type': 'KNOWLEDGE_BASE',
             'knowledgeBaseConfiguration': {
-                'knowledgeBaseId': KNOWLEDGE_BASE_ID,
+                'knowledgeBaseId': KNOWLEDGE_BASE_ID2,
                 'modelArn': MODEL_ARN,
             }
         }
@@ -34,11 +36,54 @@ def get_rag_response(query):
 
 
 # retrieve + converse 활용 
+
+def get_rag_response1(query):
+
+    # Bedrock KB에서 OpenSearch 검색 실행
+    search_response = kb_client.retrieve(
+        knowledgeBaseId=KNOWLEDGE_BASE_ID1,
+        retrievalQuery={"text": query},
+        retrievalConfiguration={
+        'vectorSearchConfiguration': {
+            'numberOfResults': 100
+        },
+        }
+    )
+
+    # 검색된 문서 확인
+    retrieved_docs = search_response.get("retrievalResults", [])
+
+    if not retrieved_docs:
+        print("답변: 충분한 정보가 없습니다.")
+    else:
+        # 검색된 문서를 하나의 텍스트로 변환
+        search_text = "\n".join([doc['content']['text'] for doc in retrieved_docs])
+
+        message = {
+        "role": "user",
+        "content": [
+            { "text": query },
+            { "text": "You are an AWS Monitoring expert. Based on the following contents(json type logs of Amazon Bedrock), please answer the query above:" },
+            { "text": search_text },
+            { "text": "Provide a detailed, technical, yet easy to understand response. Answer in Korean."}
+        ]
+    }
+
+        # converse API를 사용해 Nova 모델에게 검색 결과를 전달
+        response = bedrock_client.converse(
+            modelId=MODEL_ID,
+            messages=[message]
+        )
+
+        # LLM 응답 출력
+        return response['output']['message']['content'][0]['text'], search_text
+
+
 def get_rag_response2(query):
 
     # Bedrock KB에서 OpenSearch 검색 실행
     search_response = kb_client.retrieve(
-        knowledgeBaseId=KNOWLEDGE_BASE_ID,
+        knowledgeBaseId=KNOWLEDGE_BASE_ID2,
         retrievalQuery={"text": query},
     )
 
@@ -54,9 +99,9 @@ def get_rag_response2(query):
         message = {
         "role": "user",
         "content": [
-            { "text": search_text },
-            { "text": "You are an AWS expert. Based on the content above, please answer the following question:" },
             { "text": query },
+            { "text": "You are an AWS expert. Based on the following contents, please answer the above question:" },
+            { "text": search_text },
             { "text": "Provide a detailed, technical, yet easy to understand response. Answer in Korean."}
         ]
     }
